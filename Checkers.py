16e1fa3cfb4c331d1fce3f378fdc0d81b3bd4482
Tim Franklin <tim@pelican.org>
2019-11-13 21:38:10 +0000

    Test for nested patterns.

--- Checkers.py
+++ Checkers.py
@@ -12,8 +12,15 @@ class Checkers(Pattern):
     def pattern(self, point):
         # Incoming point has already been transformed into pattern space
         # by the parent Pattern class
+        # However, we want the nested pattern to go from 0 to 1 within each
+        # check, so we need to translate the point again before passing to
+        # the inner function
         if (math.floor(point.x) + math.floor(point.y) + math.floor(point.z)) \
          % 2 == 0:
-            return self.a.pattern_at(point)
+            return self.a.pattern_at(point.translate(-math.floor(point.x),
+                                                     -math.floor(point.y),
+                                                     -math.floor(point.z)))
         else:
-            return self.b.pattern_at(point)
+            return self.b.pattern_at(point.translate(-math.floor(point.x),
+                                                     -math.floor(point.y),
+                                                     -math.floor(point.z)))
